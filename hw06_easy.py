597a5d0a9faa541a12c5dbfce5ca738cbf2514b9
Gitforfun <ultradey@mail.ru>
2019-04-21 17:42:09 +0300

    Сделал ДЗ easy. Не тестировал на правильность геометрических формул

--- hw06_easy.py
+++ hw06_easy.py
@@ -5,10 +5,13 @@ import math
 
 class Triangle:
     # функция-конструктор - запускается автоматически при создании объекта (экземпляра класса)
+
     def __init__(self, point_a: tuple, point_b: tuple, point_c: tuple):
         self.point_a = point_a
         self.point_b = point_b
         self.point_c = point_c
+
+    # методы:
 
     def lines(self):
         a_b = math.sqrt(abs(self.point_a[0] - self.point_b[0]) ** 2 + abs(self.point_a[1] - self.point_b[1]) ** 2)
@@ -17,17 +20,17 @@ class Triangle:
         return a_b, b_c, c_a
 
     def perimetr(self):
-        perimetr = 0
+        perim_triang = 0
         lines_list = self.lines()
         for lin in lines_list:
-            perimetr = perimetr + lin
-        return perimetr
+            perim_triang += lin
+        return perim_triang
 
     def square(self):
         # 1/2 * h_a * b_c
         lines_list = self.lines()
-        p = (self.perimetr())/2
-        h_bc = (2 * math.sqrt(p * (p - lines_list[1])*(p - lines_list[2])*(p - lines_list[0])))/ lines_list[1]
+        p = (self.perimetr()) / 2
+        h_bc = (2 * math.sqrt(p * (p - lines_list[1]) * (p - lines_list[2]) * (p - lines_list[0]))) / lines_list[1]
         return h_bc * lines_list[1] / 2
 
 
@@ -36,12 +39,13 @@ print(treugol_1.perimetr())
 print(treugol_1.lines())
 print(treugol_1.square())
 
+
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
-# условия ввода точек: верхнее основание - отрезок BC, нижнее основание - отрезок AC
+# условия ввода точек: верхнее основание - отрезок BC, нижнее основание - отрезок AD
 class Isosceles_trapezoid:
     def __init__(self, point_a: tuple, point_b: tuple, point_c: tuple, point_d: tuple):
         self.point_a = point_a
@@ -49,4 +53,33 @@ class Isosceles_trapezoid:
         self.point_c = point_c
         self.point_d = point_d
 
-        print()
+    def lines(self):
+        a_b = math.sqrt(abs(self.point_a[0] - self.point_b[0]) ** 2 + abs(self.point_a[1] - self.point_b[1]) ** 2)
+        b_c = math.sqrt(abs(self.point_b[0] - self.point_c[0]) ** 2 + abs(self.point_b[1] - self.point_c[1]) ** 2)
+        c_d = math.sqrt(abs(self.point_c[0] - self.point_d[0]) ** 2 + abs(self.point_c[1] - self.point_d[1]) ** 2)
+        d_a = math.sqrt(abs(self.point_d[0] - self.point_a[0]) ** 2 + abs(self.point_d[1] - self.point_a[1]) ** 2)
+        return a_b, b_c, c_d, d_a
+
+    def tru_trap(self):
+        lines_trap = self.lines()
+        return lines_trap[0] == lines_trap[2]
+
+    def perimetr(self):
+        p_trap = 0
+        lines_list = self.lines()
+        for lin in lines_list:
+            p_trap += lin
+        return p_trap
+
+    def square(self):
+        lines_trap = self.lines()
+        h_trap = math.sqrt(4 * (lines_trap[0] ** 2) - (abs(lines_trap[1] - lines_trap[3])) ** 2) / 2
+        square_trap = ((lines_trap[1] + lines_trap[3]) / 2) * h_trap
+        return square_trap
+
+
+new_trap = Isosceles_trapezoid((1, 1), (2, 5), (6, 5), (7, 1))
+print(new_trap.tru_trap())
+print(new_trap.lines())
+print(new_trap.perimetr())
+print(new_trap.square())
